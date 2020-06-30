fd492aa058d372e7ee3281f83e81fa522539628b
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2020-06-30 17:57:19 +0900

    プログラムを追加

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("プログラムを起動するためには，ターミナルから以下のように入力してください");
+console.log("（ただし「>」はプロンプトを表すので入力しないこと）");
+console.log("> node nibun.js");
+console.log("　または");
+console.log("> node newton.js");
